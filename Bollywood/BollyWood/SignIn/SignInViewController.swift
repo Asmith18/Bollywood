2664b51e3c361db6b8be13c39db5f9ef65ad0f7c
Asmith18 <Smittysadam@gmail.com>
2022-07-18 13:58:16 -0500

    sign in and sign up vc done.

--- Bollywood/BollyWood/SignIn/SignInViewController.swift
+++ Bollywood/BollyWood/SignIn/SignInViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Actions
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        if let email = emailTextField.text,
+           let password = passwordTextField.text {
+            FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                switch result {
+                    
+                case .none:
+                    let alertController = UIAlertController(title: "Account not found", message: "Please check your Email and Password", preferredStyle: .alert)
+                    let confirmAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(confirmAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                case .some(let userDetails):
+                    UserDefaults.standard.set(true, forKey: "signedInWithFirebase")
+                    print("signed in as", userDetails.user.email!)
+                    UserDefaults.standard.set(userDetails.user.uid, forKey: "uid")
+                    UserDefaults.standard.set(userDetails.user.email, forKey: "email")
+                    let storyboard = UIStoryboard(name: "Account", bundle: nil)
+                    guard let viewController = storyboard.instantiateViewController(withIdentifier: "account") as? AccountViewController else { return }
+                    self.present(viewController, animated: true)
+                }
+            }
+        }
     }
-    */
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: "signup") as? SignUpViewController else { return }
+        self.navigationController?.pushViewController(viewController, animated: false)
+    }
+    
+    func validatePassword(_ password: String) -> Bool {
 
+        if password.count < 6 {
+            return false
+        }
+        if password.range(of: #"\s+"#, options: .regularExpression) != nil {
+            return false
+        }
+            return true
+    }
 }
---
+++ Bollywood/BollyWood/SignUp/SignUpViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SignUpViewController.swift
+//  Bollywood
+//
+//  Created by adam smith on 7/18/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        if emailTextField.text?.isEmpty == true {
+            print("No Text entered in email field")
+            let alertController = UIAlertController(title: "Error: Email Address Field Empty", message: "Please Enter a Valid Email Address", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(confirmAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        if passwordTextField.text?.isEmpty == true {
+            print("No Text Entered in Password Field")
+            let alertController = UIAlertController(title: "Error: Password Field is Empty", message: "Please Enter a Valid Password", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(confirmAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        guard let password = passwordTextField.text else { return }
+        if validatePassword(password) == false {
+            let alertController = UIAlertController(title: "Password is Invalid", message: "Password must contain a minimum of 6 characters and no spaces. Please try again.", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(confirmAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        signUp()
+    }
+    
+    func signUp() {
+        if let email = emailTextField.text,
+           let password = passwordTextField.text {
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                
+                guard let user = authResult?.user else { return }
+                UserDefaults.standard.set(true, forKey: "signedInWithFirebase")
+                UserDefaults.standard.set(user.uid, forKey: "uid")
+                UserDefaults.standard.set(user.email, forKey: "email")
+                let storyboard = UIStoryboard(name: "Account", bundle: nil)
+                guard let viewController = storyboard.instantiateViewController(withIdentifier: "account") as? AccountViewController else { return }
+                self.present(viewController, animated: true)
+            }
+        }
+    }
+    
+    func validatePassword(_ password: String) -> Bool {
+
+        if password.count < 6 {
+            return false
+        }
+        if password.range(of: #"\s+"#, options: .regularExpression) != nil {
+            return false
+        }
+            return true
+    }
+}
